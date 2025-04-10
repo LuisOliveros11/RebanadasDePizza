084e8599636a00abdcbc0c3fa5eef7c3d9471a70
Kev1n011 <90216024+Kev1n011@users.noreply.github.com>
2025-04-10 01:50:16 -0700

    Algoritmo Bresenham y botón para actualizar las rebanadas

--- sketch.js
+++ sketch.js
@@ -1,19 +1,36 @@
-let cantidadRebanadas = 3;
+let cantidadRebanadas = 0;
 let diametroPizza = 200;
 let xPizza1 = 200;
 let yPizza1 = 200;
 let xPizza2 = 500;
 let yPizza2 = 300;
+let xPizza3 = 800;
+let yPizza3 = 400;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1200, 1200);
+
+  inputRebanadas = createInput(cantidadRebanadas.toString());
+  inputRebanadas.position(10, 10);
+  inputRebanadas.size(100);
+
+  botonActualizar = createButton('Actualizar rebanadas');
+  botonActualizar.position(inputRebanadas.x + inputRebanadas.width + 10, 10);
+  botonActualizar.mousePressed(actualizarRebanadas);
 }
 
+function actualizarRebanadas() {
+  let valor = int(inputRebanadas.value());
+  if (!isNaN(valor) && valor > 0) {
+    cantidadRebanadas = valor;
+  }
+}
 function draw() {
   background(220);
   
    circle(xPizza1, yPizza1, diametroPizza);
    circle(xPizza2, yPizza2, diametroPizza);
+   circle(xPizza3, yPizza3, diametroPizza);
   
   
   //FOR ALGORITMO PP
@@ -35,6 +52,17 @@ function draw() {
     let yFinal = round(yPizza2 + (diametroPizza / 2) * sin(anguloRebanada));
     
     algoritmoDDA(xPizza2, xFinal, yPizza2, yFinal);
+  }
+  
+  //FOR ALGORITMO Bresenham 
+  for (let i = 0; i < cantidadRebanadas; i++){
+    let anguloRebanada = i * ((2 * PI) / cantidadRebanadas);
+    
+    let xFinal = round(xPizza3 + (diametroPizza / 2) * cos(anguloRebanada));
+    let yFinal = round(yPizza3 + (diametroPizza / 2) * sin(anguloRebanada));
+    
+     algoritmoBresenham(xPizza3, yPizza3, xFinal, yFinal);
+    
   }
   
 }
@@ -91,3 +119,62 @@ function algoritmoDDA(x1, x2, y1, y2){
     y1 += incrementoY;
   }
 }
+
+function algoritmoBresenham(x0, y0, x1, y1) {
+  if (abs(y1 - y0) < abs(x1 - x0)) {
+    lineaHorizontal(x0, y0, x1, y1); 
+  } else {
+    lineVertical(x0, y0, x1, y1); 
+  }
+}
+
+function lineaHorizontal(x0, y0, x1, y1) {
+  if (x0 > x1) {
+    [x0, x1] = [x1, x0];
+    [y0, y1] = [y1, y0];
+  }
+
+  let dx = x1 - x0;
+  let dy = y1 - y0;
+  let dir = (dy < 0) ? -1 : 1;
+  dy *= dir;
+
+  if (dx !== 0) {
+    let y = y0;
+    let p = 2 * dy - dx;
+    for (let i = 0; i <= dx; i++) {
+      point(x0 + i, y);
+      if (p >= 0) {
+        y += dir;
+        p -= 2 * dx;
+      }
+      p += 2 * dy;
+    }
+  }
+}
+
+function lineVertical(x0, y0, x1, y1) {
+  if (y0 > y1) {
+    [x0, x1] = [x1, x0];
+    [y0, y1] = [y1, y0];
+  }
+
+  let dx = x1 - x0;
+  let dy = y1 - y0;
+  let dir = (dx < 0) ? -1 : 1;
+  dx *= dir;
+
+  if (dy !== 0) {
+    let x = x0;
+    let p = 2 * dx - dy;
+    for (let i = 0; i <= dy; i++) {
+      point(x, y0 + i);
+      if (p >= 0) {
+        x += dir;
+        p -= 2 * dy;
+      }
+      p += 2 * dx;
+    }
+  }
+}
+
